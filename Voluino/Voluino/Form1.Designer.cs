34eb978f79b237c71043e4195f453959a2c52133
xicocana <36681727+xicocana@users.noreply.github.com>
2019-01-26 15:56:11 +0000

    Add files via upload

---
+++ Voluino/Voluino/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Voluino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Volume:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Device:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(63, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM13";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(253, 46);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Voluino";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Voluino/Voluino/Form1.cs
@@ -0,0 +1,84 @@
+﻿using AudioSwitcher.AudioApi.CoreAudio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Voluino
+{
+    public partial class Form1 : Form
+    {
+
+        String msg;
+        string[] tokens;
+        IEnumerable<CoreAudioDevice> devices = new CoreAudioController().GetPlaybackDevices();
+
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1.Open();
+            timer1.Enabled = true;
+
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            //handling of the msg
+            msg = serialPort1.ReadLine();
+            tokens = msg.Split('%');
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label3.Text = tokens[0];
+
+            foreach (CoreAudioDevice d in devices)
+            {
+                if (d.IsDefaultDevice)
+                    d.Volume = Int32.Parse(tokens[0]);
+            }
+
+            if (tokens[1].Length == 3)
+            {
+                if (isDef("Headphones (2- Razer Kraken USB)") == false)
+                {
+                    ChangeOutput("Headphones (2- Razer Kraken USB)");
+                    label4.Text = "Headphones (2- Razer Kraken USB)";
+                }
+            }
+            if (tokens[1].Length == 4)
+            {
+                if (isDef("Speakers (Realtek High Definition Audio)") == false)
+                {
+                    ChangeOutput("Speakers (Realtek High Definition Audio)");
+                    label4.Text = "Speakers (Realtek High Definition Audio)";
+                }
+            }
+        }
+
+        private void ChangeOutput(string op)
+        {
+            foreach (CoreAudioDevice d in devices)
+            {
+                if (d.FullName == op)
+                    d.SetAsDefault();
+            }
+        }
+
+        private bool isDef(string op)
+        {
+            foreach (CoreAudioDevice d in devices)
+            {
+                if (d.IsDefaultDevice && d.FullName == op)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
